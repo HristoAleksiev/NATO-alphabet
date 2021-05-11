4a3071e81f10d5f242b8a023991643ffc571b540
Алексиев <kadoll555@gmail.com>
2021-05-11 22:26:04 +0300

    added an exception handling block

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import pandas
 
+
 nato_alphabet_dict = {value.letter: value.code for (key, value) in
                       pandas.read_csv("nato_phonetic_alphabet.csv").iterrows()}
-word_in_nato = [nato_alphabet_dict[letter] for letter in input("Please say your word: ").upper()]
 
-print(word_in_nato)
+word_in_nato = None
+
+while word_in_nato is None:
+    try:
+        word_in_nato = [nato_alphabet_dict[letter] for letter in input("Please say your word: ").upper()]
+    except KeyError:
+        print("Sorry, only letter of the alphabet are allowed!")
+    else:
+        print(word_in_nato)
